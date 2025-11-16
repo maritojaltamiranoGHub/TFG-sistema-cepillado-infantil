8b0d0c7800eff2b45a5353964b559891bd4fd234
maritojaltamiranoGHub <maritojaltamirano@gmail.com>
2025-11-16 15:51:23 -0300

    Carga inicial del prototipo - TFG

---
+++ Sistema.Cepillado.Aplicacion/Servicios/ServicioPerfilNino.cs
@@ -0,0 +1,71 @@
+﻿using Sistema.Cepillado.Dominio.Interfaces;
+using Sistema.Cepillado.Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Cepillado.Aplicacion.Servicios
+{
+    public class ServicioPerfilNino : IServicioPerfilNino
+    {
+
+		private readonly IRepositorioTutor _repositorioTutor;
+		public ServicioPerfilNino(IRepositorioTutor repositorioTutor)
+		{
+			_repositorioTutor = repositorioTutor;
+		}
+
+
+		public Nino ActualizarPerfilNino(Nino nino)
+        {
+			//actualizo el perfil del tutor
+
+			bool ninoActualizado;
+
+			ninoActualizado = _repositorioTutor.ActualizarNino(nino);
+
+			if (ninoActualizado)
+			{
+				return nino;
+			}
+			else
+			{
+				return new Nino();
+
+			}
+		}
+
+        public Nino CrearPerfilNino(Nino nino, Tutor tutor)
+        {
+
+			bool ninoRegistrado;
+
+			ninoRegistrado = _repositorioTutor.CrearNinoParaTutor(nino, tutor.Id);
+
+			if (ninoRegistrado)
+			{
+				return nino;
+			}
+			else
+			{
+				return new Nino();
+
+			}
+
+		}
+
+        public void EliminarPerfilNino(int ninoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Nino> ObtenerPerfilNinoPorId(int ninoId)
+        {
+			//a travez del repositorio obtener el perfil del niño
+			return  _repositorioTutor.ObtenerNinoPorId(ninoId);
+
+		}
+
+       
+    }
+}
---
+++ Sistema.Cepillado.Aplicacion/Servicios/ServicioTutores.cs
@@ -0,0 +1,77 @@
+﻿using Sistema.Cepillado.Dominio.Interfaces;
+using Sistema.Cepillado.Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Cepillado.Aplicacion.Servicios
+{
+    public class ServicioTutores : IServicioTutores
+    {
+
+        private readonly IRepositorioTutor _repositorioTutor;
+        public ServicioTutores(IRepositorioTutor repositorioTutor)
+        {
+            _repositorioTutor = repositorioTutor;
+		}
+
+		public Tutor RegistrarTutor(Tutor tutor)
+		{
+			bool tutorRegistrado;
+
+			tutorRegistrado = _repositorioTutor.RegistrarTutor(tutor);
+
+			if (tutorRegistrado)
+			{
+				return tutor;
+			}
+			else
+			{
+				return new Tutor();
+
+			}
+		}
+
+		public async Task<Tutor?> LoginTutor(string email, string password)
+        {
+
+		
+            return await _repositorioTutor.LoginTutor(email, password);
+
+		}
+		//busco el tutor por id
+		public async Task<Tutor> ObtenerPerfilTutorPorId(int tutorId)
+		{
+			return await _repositorioTutor.ObtenerTutorPorId(tutorId);
+
+		}
+
+		public List<Nino> ObtenerNinosPorTutor(int tutorId)
+        {
+            return _repositorioTutor.ObtenerNinosPorTutor(tutorId);
+
+		}
+
+		public  Tutor ActualizarPerfilTutor(Tutor tutor)
+		{
+			//actualizo el perfil del tutor
+			 
+			bool tutorActualizado;
+
+			tutorActualizado = _repositorioTutor.ActualizarTutor(tutor);
+
+			if (tutorActualizado)
+			{
+				return tutor;
+			}
+			else
+			{
+				return new Tutor();
+
+			}
+
+		}
+
+        
+    }
+}
