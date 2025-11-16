62c5f0af580487950cddb0096d4dd85ca37c2fc8
maritojaltamiranoGHub <maritojaltamirano@gmail.com>
2025-11-16 16:01:03 -0300

    .

---
+++ Sistema.Cepillado.Presentacion/Compartido/Estado.cs
@@ -0,0 +1,45 @@
+﻿using Sistema.Cepillado.Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Cepillado.Presentacion.Compartido
+{
+	public static class Estado 
+	{
+		
+		private static Tutor? _tutorActivo;
+		private static Nino? _ninoActivo;
+
+		
+		public static Tutor? GetTutorActivo()
+		{
+			return _tutorActivo;
+		}
+
+		
+		public static void SetTutorActivo(Tutor tutor)
+		{
+			
+			_tutorActivo = tutor;
+		}
+
+
+		public static Nino? GetNinoActivo()
+		{
+			return _ninoActivo;
+		}
+
+		public static void SetNinoActivo(Nino nino)
+		{
+			_ninoActivo = nino;
+		}
+
+		// (RESET)
+		public static void LimpiarEstado()
+		{
+			_tutorActivo = null;
+			_ninoActivo = null;
+		}
+	}
+}
---
+++ Sistema.Cepillado.Presentacion/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sistema.Cepillado.Presentacion
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
